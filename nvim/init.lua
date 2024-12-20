f36f0e36f57c7c3d4cd4ea4d29205a9ae55245cf
Nikita <github@nikiv.dev>
2024-12-20 21:01:02 +0100

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,4 +6,14 @@
 -- TODO: get ghostty theme that is github theme like in cursor. same for neovim (should be identical)
 -- TODO: bind to reload current nvim editor instance from config. to fast iterate
 
+-- remove line numbers on side
 vim.opt.number = false
+
+-- remove status bar on bottom
+vim.opt.laststatus = 0
+
+-- hide ~ ~ on non used lines
+vim.opt.fillchars:append({ eob = " " })
+
+-- reload config with space+f
+vim.keymap.set('n', '<Space>w', ':w<CR>:source $MYVIMRC<CR>', { noremap = true, silent = true })
